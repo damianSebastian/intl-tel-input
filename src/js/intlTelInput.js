bc8845246710f5e49ff115a7659968b1663c18c2
Avram Sebastian <avramsebastian64@gmail.com>
2022-02-22 21:33:39 +0200

    ro+us onlyCountry

--- src/js/intlTelInput.js
+++ src/js/intlTelInput.js
@@ -40,11 +40,11 @@ const defaults = {
   // don't insert international dial codes
   nationalMode: true,
   // display only these countries
-  onlyCountries: [],
+  onlyCountries: ['ro', 'us'],
   // number type to use for placeholders
   placeholderNumberType: 'MOBILE',
   // the countries at the top of the list. defaults to united states and united kingdom
-  preferredCountries: ['ro','us'],
+  preferredCountries: ['ro'],
   // display the country dial code next to the selected flag so it's not part of the typed number
   separateDialCode: false,
   // specify the path to the libphonenumber script to enable validation/formatting
